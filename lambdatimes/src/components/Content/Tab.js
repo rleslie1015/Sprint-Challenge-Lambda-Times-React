9d32a8e1f3cfada3dd5e350796cc82d4682ec352
lrod <rleslie1015@gmail.com>
2019-04-12 09:02:51 -0700

    tabs are rendering

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -2,19 +2,24 @@ import React from 'react';
 
 const Tab = props => {
   if (props.tab === props.tab.selectedTab) {
-  return (
-    <div
-      className={'tab active-tab'}
-      onClick={(tab) => {props.selectTabHandler}}
-    >
-      {props.tab.toUpperCase()}
-    </div>);
+    return (
+      <div
+        className={'tab active-tab'}
+        onClick={(tab) => {props.selectTabHandler}}
+      >
+        {props.tab.toUpperCase()}
+      </div>
+      );
   } else {
     return (
-      <div>
-        
+      <div
+        className={'tab'}
+        onClick={(tab) => { props.selectTabHandler}}
+      >
+        {props.tab.toUpperCase()}
       </div>
-    )
+      );
+    }
   }
 
 // Make sure you include PropTypes on your props.
